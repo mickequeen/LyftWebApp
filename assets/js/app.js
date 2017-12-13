345805d3234a3bb88e23ea7ba61d5e45f13758ac
mickequeen <mickequeen@gmail.com>
2017-12-13 17:26:37 -0300

    splash + loader

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  /*
+  *función de splash
+  */
+  setTimeout(function() {
+    $(".splash").fadeOut(1500);
+  },2500);
+  /*
+  *función para mostrar contenido de la página
+  */
+  setTimeout(function() {
+      $(".content").fadeIn(1500);
+  },3500);
+
+  
+});
